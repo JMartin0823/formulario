18608cf06c04a8592bb1d877418ff67f4f19c241
Juan Martin Lopez <115959645+JMartin0823@users.noreply.github.com>
2023-08-11 18:59:13 -0500

    Add files via upload

---
+++ numbers.js
@@ -0,0 +1,16 @@
+const evenNumbers = [];
+const oddNumbers = [];
+const multiplesOfThree = [];
+
+function number(n) {
+    if(n % 2==0){
+        evenNumbers.push(number)
+        console.log(evenNumbers)
+        }else{
+        oddNumbers.push(number)
+        console.log(oddNumbers)
+        }
+        
+}
+
+
